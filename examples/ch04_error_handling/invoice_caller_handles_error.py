ea1fe1840146bc911d500e1ca3552983c61a98ea
Python Developer <pydev@localhost.localdomain>
2020-01-07 21:38:15 -0500

    Added examples for chap 4 error handling

---
+++ examples/ch04_error_handling/invoice_caller_handles_error.py
@@ -0,0 +1,48 @@
+"""
+Invoice class example from chapter 4.
+Demonstrates try statement.
+"""
+
+from datetime import date
+
+
+class InvoiceDataAccessObject:
+    def __init__(self) -> None:  # set up connection to DB
+        self.usage_count = 0
+
+    def lookup_status(self, invoice_id: int) -> int:
+        if invoice_id < 1:
+            raise ValueError(f'invalid invoice_id {invoice_id}')
+
+        self.usage_count += 1
+        return self.usage_count
+
+
+class InvoiceProcessingError(Exception):
+    pass
+
+
+class Invoice:
+    invoice_id: int
+    release_cutoff: date
+    dao: InvoiceDataAccessObject
+
+    def __init__(self, invoice_id: int, cutoff: date, dao) -> None:
+        self.invoice_id = invoice_id
+        self.release_cutoff = cutoff
+        self.dao = dao
+
+    def is_invoice_released(self) -> bool:
+        try:
+            status = self.dao.lookup_status(self.invoice_id)
+            return status == 1 and self.release_cutoff >= date.today()
+        except ValueError as ve:
+            print(f'ValueError: {ve}')
+            raise InvoiceProcessingError(
+                f'problem processing invoice {self.invoice_id}')
+
+if __name__ == '__main__':
+    invoice = Invoice(0, date(2021, 12, 31), InvoiceDataAccessObject())
+    un = '' if invoice.is_invoice_released() else 'un'
+    print(f'invoice is {un}released')
+    print('Done processing invoice')
