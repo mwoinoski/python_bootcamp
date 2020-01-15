055c5e6e234b6859e8a3cbbee81c31cb7beb9b5b
Mike Woinoski <mike.woinoski@gmail.com>
2020-01-15 09:18:49 -0500

    Added files to ex 2.1 and 3.1

---
+++ exercises/ex03_oop/facilities/populate_facilities.py
@@ -0,0 +1,14 @@
+"""
+Driver for Health Care Facility application
+"""
+
+import sys
+
+
+def main(path):
+    """ Populate objects for the health care facilities """
+    pass
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
---
+++ exercises/ex03_oop/tests/test_populate_facilities.py
@@ -0,0 +1,10 @@
+"""
+Integration tests for populate_facilities.py
+"""
+
+from io import IOStream
+
+
+class TestPopulateFacilities:
+    def test_populate_facilities(self):
+        assert False
